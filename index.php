bceedf3a1b01421f17c102bcc1787afe30aaf571
shaharyar522 <124666378+shaharyar522@users.noreply.github.com>
2025-05-13 16:36:03 +0500

    working on notesapp

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (isset($_POST['submit']) && $_POST['submit'] == 'sub') {
 }
 
 
-//update code 
+//update code     
 require_once("./incs/update_code.php");
 //Delete code
 require_once("./incs/delete_code.php");
